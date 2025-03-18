906f485dae35cfc8304b1089b1343da1869fac1d
noone787 <witcherandmaster@op.pl>
2025-03-18 19:45:26 +0100

    motyka common - 10 gr

--- main.gd
+++ main.gd
@@ -1,13 +1,14 @@
-extends Node2D
+extends Control
 
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_packed(preload("res://mapa.tscn"))
+
+func _on_mapa_pressed():
+	get_tree().change_scene_to_file("res://mapa.tscn")
 
 
-func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_packed(preload("res://mapa.tscn"))
+func _on_informacje_pressed():
+	get_tree().change_scene_to_file("res://informacje.tscn")
 
 
-func _on_button_3_pressed() -> void:
-	get_tree().change_scene_to_packed(preload("res://market.tscn"))
+func _on_market_pressed():
+	get_tree().change_scene_to_file("res://market.tscn")
