805d58ac05023c1f3831926f3907d6b69693b2d6
noone787 <witcherandmaster@op.pl>
2025-03-18 20:55:14 +0100

    rhaedrth

--- main.gd
+++ main.gd
@@ -12,3 +12,7 @@ func _on_informacje_pressed():
 
 func _on_market_pressed():
 	get_tree().change_scene_to_file("res://market.tscn")
+
+
+func _on_calc_pressed():
+	pass # Replace with function body.
